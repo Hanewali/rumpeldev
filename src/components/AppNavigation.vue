77834506f9c11e900b27ebf15e65eada13c08ce1
Jakub Rumpel <jakub@life-coder.pl>
2022-04-04 18:39:52 +0200

    Added simple site navigation

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar">
+    <RouterLink to="/" class="router-link">Home</RouterLink>
+    <RouterLink to="/about" class="router-link">About</RouterLink>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AppNavigation'
+}
+</script>
+
+<style scoped lang="css">
+  .navbar>.router-link:not(:last-child){
+    border-right: 1px solid black;
+  }
+  .navbar>.router-link{
+    padding-right: 0.5rem;
+    padding-left: 0.5rem;
+  }
+</style>
