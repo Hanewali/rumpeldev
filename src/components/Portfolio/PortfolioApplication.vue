636c047cb8f5e0a389f56a97514cc9fb49df976a
Jakub Rumpel <jakub@life-coder.pl>
2022-04-07 21:15:05 +0200

    Added PortfoltioApplication component

---
+++ src/components/Portfolio/PortfolioApplication.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="portfolio">
+    <img src="http://via.placeholder.com/150x80" alt="Placeholder">
+    <h4>AppName</h4>
+    <p>This is application description</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+export default {
+  name: 'PortfolioApplication',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .portfolio{
+    min-width: 300px;
+    display: inline-block;
+    background-color: #00000010;
+    min-width: content-box/4;
+    padding: 2rem;
+  }
+</style>
