1893c386f379aea7a0b881ce85ee20a3f48f73e9
Jakub Rumpel <jakub@life-coder.pl>
2022-04-11 16:33:01 +0200

    Added basic BlogPost component content

--- src/components/Blog/BlogPost.vue
+++ src/components/Blog/BlogPost.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="blogpost">
-
+    <img :src="imageUrl" alt="Post image">
+    <div class="postInfo">
+      <h3>{{ title }}</h3>
+      <p>{{ contentSummary }}</p>
+    </div>
   </div>
 </template>
 
@@ -13,5 +17,20 @@ defineProps({
 </script>
 
 <style scoped lang="scss">
+img {
+  width: 100%;
+  height: 100%;
+}
 
+.postInfo{
+  position: absolute;
+  margin-top: auto;
+  margin-bottom: 10%;
+}
+
+.blogpost {
+  width: 60vw;
+  margin-right: auto;
+  margin-left: auto;
+}
 </style>
