2a1884f9bb2477304525122526dab82c4ec36d9f
Jakub Rumpel <jakub@life-coder.pl>
2022-04-11 15:47:37 +0200

    Created basic view and component for blog view

---
+++ src/components/Blog/BlogPost.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="blogpost">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String,
+  contentSummary: String,
+  imageUrl: String
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
